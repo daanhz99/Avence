b0c6a099f35fac079b783bcfc49cf365a397ee35
Guus Huizen <guus@snuffelbox.nl>
2019-05-14 11:30:42 +0200

    * Changed form submission type to use 3rd party

--- resources/views/portal/pages/testimonials.blade.php
+++ resources/views/portal/pages/testimonials.blade.php
@@ -18,14 +18,26 @@
                 @endif
 
                 <div class="form-style-8">
-                    <form method="post" action="/testimonials">
-                        {{ csrf_field() }}
-                        <input type="text" placeholder="Full Name" required name="full_name" />
-                        <input type="email" placeholder="Email" required name="email" />
-                        <input type="text" placeholder="Company Name" required name="company_name" />
-                        <textarea placeholder="Your experience with Avence" required name="experience"></textarea>
-                        <input class="button-blue hvr-shutter-out-horizontal" type="submit" value="Send Message"/>
+{{--                    <form method="post" action="/testimonials">--}}
+{{--                        {{ csrf_field() }}--}}
+{{--                        <input type="text" placeholder="Full Name" required name="full_name" />--}}
+{{--                        <input type="email" placeholder="Email" required name="email" />--}}
+{{--                        <input type="text" placeholder="Company Name" required name="company_name" />--}}
+{{--                        <textarea placeholder="Your experience with Avence" required name="experience"></textarea>--}}
+{{--                        <input class="button-blue hvr-shutter-out-horizontal" type="submit" value="Send Message"/>--}}
+{{--                    </form>--}}
+
+                    <form action="https://formcarry.com/s/R5DAnjSeqdj" method="POST" accept-charset="UTF-8" >
+                        <input type="text" name="full_name" placeholder="Full name">
+                        <input type="email" name="email" placeholder="Email">
+                        <textarea name="message" placeholder="Your experience with Avence"></textarea>
+                        <input type="hidden" name="_subject" value="Nieuwe testimonial">
+                        <input type="hidden" name="_after" value="http://avence.nl/testimonials?sent=true">
+                        <input type="hidden" name="_honeypot" value="">
+                        <input type="hidden" name="_confirmation" value="">
+                        <input type="submit" class="button-blue hvr-shutter-out-horizontal" value="Send Message">
                     </form>
+
                 </div>
             </div>
         </div>
